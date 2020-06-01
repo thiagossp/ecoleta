129e3f04f4bf25d4cc007bd617e975b4c2e369f3
Thiago da Silva Pinto <email.thiagosp@gmail.com>
2020-06-01 14:39:21 -0300

    Dia 1

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('Listagem de usuários');
+
+    response.json([
+        'Thiago',
+        'Diego'
+    ])
+});
+
+app.listen(3333);
