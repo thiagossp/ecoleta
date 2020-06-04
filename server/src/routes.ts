95d2fa98eec9de314bbf6222c8ea8efe2633cea4
Thiago da Silva Pinto <email.thiagosp@gmail.com>
2020-06-04 11:01:09 -0300

    Dia 2

---
+++ server/src/routes.ts
@@ -0,0 +1,19 @@
+import express, { request, response } from 'express';
+
+import PointsController from './controllers/PointsControllers';
+import ItemsController from './controllers/ItemsControllers';
+
+// index, show, create, updade, delete
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+const routes = express.Router();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+export default routes;
